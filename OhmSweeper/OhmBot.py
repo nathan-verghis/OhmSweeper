6535fbb7d1afb60cd17b42ec40b48cdbc4dd5c5c
Nathan Verghis <nbverghis@gmail.com>
2020-08-08 14:06:46 -0400

    updates

--- OhmSweeper/OhmBot.py
+++ OhmSweeper/OhmBot.py
@@ -5,29 +5,40 @@ import time
 
 class OhmBot:
     def __init__(self):
-        self.bot = webdriver.Chrome()
+        self.bot = webdriver.Chrome(ChromeDriverManager().install())
         self.log_history = []
 
     def store_reply(self):
-        self.log_history.append(self.bot.find_elements_by_class_name("logitem")[-1].text)
+        if self.log_history[-1] != self.bot.find_elements_by_class_name("logitem")[-1].text:
+            self.log_history.append(self.bot.find_elements_by_class_name("logitem")[-1].text)
 
     def get_last_input(self):
-        return self.log_history[-1].text
+        return self.log_history[-1]
 
-    def is_human(self):
-        if type_speed(self.get_last_input(), self.type_time()) > 12.5:
+    def is_human(self, type_time):
+        if type_speed(self.get_last_input(), type_time) > 12.5:
             return False
         else:
             return True
 
-    def type_time(self):
-        if self.get_last_input() == "Stranger is typing":
-            time_start = time.time()
-        if self.get_last_input() != "Stranger is typing":
-            time_end = time.time()
-        return time_end - time_start
+    def type_time(self, time1):
+        self.wait_for_message()
+        time_end = float(time.time())
+        return time_end - time1
 
-    def communicate:
+    def wait_for_message(self):
+        while self.log_history[-2] != "Stranger is typing":
+            pass
+        time.sleep(2)
+
+    def communicate(self):
+        while not self.chat_is_over():
+            current_message = self.bot.find_elements_by_class_name("logitem")[-1].text
+            if current_message == "Stranger is typing":
+                time_start = float(time.time())
+                type_time = self.type_time(time_start)
+
+
 
 
     def identify_predator:
@@ -48,19 +59,12 @@ class OhmBot:
 def type_speed(message, time):
     length = len(message)
     wpm = length / time
+    return wpm
 
 def timer
 
 def is_typing():
 
-connected = True
-
-while connected:
-    typing = is_typing()
-    if typing:
-        timer.start
-    if message_received:
-        timer.stop
 '''
 bot = webdriver.Firefox()
 possible_endings =\
