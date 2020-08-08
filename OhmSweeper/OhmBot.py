dbbee6eefd2831e0d4fa89cad5a76fb8efb26181
Matthew Szurkowski <MatthewSzurkowski@Matthews-Mac-mini.local>
2020-08-08 13:15:49 -0400

    Added newchat

--- OhmSweeper/OhmBot.py
+++ OhmSweeper/OhmBot.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 import time
+import linkSender as sender
 
 
 class OhmBot:
     def __init__(self):
-        self.bot = webdriver.Chrome()
+        self.bot = webdriver.Chrome(ChromeDriverManager().install())
         self.log_history = []
 
     def get_reply(self):
@@ -16,15 +18,18 @@ class OhmBot:
         return self.bot.find_elements_by_class_name("logitem")[-1].text
 
     def is_human(self):
+        pass
+
+    def communicate():
+        pass
 
 
-    def communicate:
+    def identify_predator():
+        pass
 
 
-    def identify_predator:
-
-
-    def upload_predator:
+    def upload_predator():
+        pass
 
     def chat_is_over(self):
         possible_endings = \
@@ -35,39 +40,19 @@ class OhmBot:
         else:
             return False
 
+    def newchat(self):
+        self.bot.get("https://www.omegle.com/")
+        textButton = self.bot.find_element_by_xpath("//img[@id='textbtn']")
+        textButton.click()
+        time.sleep(3)
+        
+        
+        
+        
 
-def type_speed(message, time):
-    length = len(message)
-    wpm = length / time
-
-def timer
-
-statuslog
-def is_typing():
-
-connected = True
-
-while connected:
-    typing = is_typing()
-    if typing:
-        timer.start
-    if message_received:
-        timer.stop
-'''
-bot = webdriver.Firefox()
-possible_endings =\
-    ["Great chat? Save the log: Get a link • Select all • Or post log to: Facebook • Tumblr • Twitter • reddit",
-     "Stranger has disconnected", "Find strangers with common interests (Enable)"]
-bot.get("https://www.omegle.com")
-newchat = bot.find_element_by_id("chattypetextcell")
-newchat.click()
-time.sleep(5)
-logbox = []
-while bot.find_elements_by_class_name("logitem")[-1].text not in possible_endings:
-    time.sleep(2)
-    if bot.find_elements_by_class_name("logitem")[-1].text not in logbox:
-        logbox.append(bot.find_elements_by_class_name("logitem")[-1].text)
-
-logbox.remove(logbox[len(logbox) - 1])
-for chat in logbox:
-    print(chat)'''
+bot = OhmBot()
+bot.newchat()
+while True:
+    time.sleep(1)
+    if (bot.chat_is_over() == True):
+        bot.newchat()
--- OhmSweeper/linkSender.py
+++ OhmSweeper/linkSender.py
@@ -2,14 +2,6 @@ from selenium import webdriver
 import time
 import random
 
-driver = webdriver.Chrome("/Users/MatthewSzurkowski/Desktop/ohmSweeper/chromedriver")
-driver.get("https://www.omegle.com/")
-textButton = driver.find_element_by_xpath("//img[@id='textbtn']")
-print(textButton)
-textButton.click()
-time.sleep(3)
-
-
 def sendMessage(message):
     chatBox = driver.find_element_by_tag_name('textarea')
     print(chatBox)
